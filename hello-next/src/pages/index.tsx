f31bf92b576a5a8193b081df57389ae99bbd860a
Devrath Rana <devrath.rana@kellton.com>
2023-07-31 02:10:49 +0530

    fix

--- hello-next/src/pages/index.tsx
+++ hello-next/src/pages/index.tsx
@@ -1,8 +1,9 @@
+import styles from "../styles/main.module.scss";
 export const Description = () => {
   return (
     <>
       <h1>Description</h1>
-      <style jsx global>
+      <style jsx>
         {`
           h1 {
             color: red;
@@ -16,7 +17,7 @@ export const Description = () => {
 export default function Home() {
   return (
     <>
-      <h1>Hello</h1>
+      <h1 className={styles.hello_text}>Hello <span>green</span></h1>
       <Description />
     </>
   );
---
+++ hello-next/src/pages/page1/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./index.module.scss";
+
+const Page1 = () => {
+  return <div className={styles.divc}>Page1</div>;
+};
+
+export default Page1;
